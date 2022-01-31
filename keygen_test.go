089ed92e4aeee5ce5837aa66381acf83378a2095
Carlos Alexandro Becker <caarlos0@users.noreply.github.com>
2022-01-31 12:28:23 -0300

    feat: generate ecdsa keys (#3)
    * feat: generate ecdsa keys
    
    Signed-off-by: Carlos A Becker <caarlos0@gmail.com>
    
    * chore: rename

--- keygen_test.go
+++ keygen_test.go
@@ -80,6 +80,72 @@ func TestGenerateEd25519Keys(t *testing.T) {
 	})
 }
 
+func TestGenerateECDSAKeys(t *testing.T) {
+	// Create temp directory for keys
+	dir := t.TempDir()
+
+	k := &SSHKeyPair{
+		KeyDir:   dir,
+		Filename: "test",
+	}
+
+	t.Run("test generate SSH keys", func(t *testing.T) {
+		err := k.generateECDSAKeys()
+		if err != nil {
+			t.Errorf("error creating SSH key pair: %v", err)
+		}
+
+		// TODO: is there a good way to validate these? Lengths seem to vary a bit,
+		// so far now we're just asserting that the keys indeed exist.
+		if len(k.PrivateKeyPEM) == 0 {
+			t.Error("error creating SSH private key PEM; key is 0 bytes")
+		}
+		if len(k.PublicKey) == 0 {
+			t.Error("error creating SSH public key; key is 0 bytes")
+		}
+	})
+
+	t.Run("test write SSH keys", func(t *testing.T) {
+		k.KeyDir = filepath.Join(dir, "ssh1")
+		if err := k.prepFilesystem(); err != nil {
+			t.Errorf("filesystem error: %v\n", err)
+		}
+		if err := k.WriteKeys(); err != nil {
+			t.Errorf("error writing SSH keys to %s: %v", k.KeyDir, err)
+		}
+		if testing.Verbose() {
+			t.Logf("Wrote keys to %s", k.KeyDir)
+		}
+	})
+
+	t.Run("test not overwriting existing keys", func(t *testing.T) {
+		k.KeyDir = filepath.Join(dir, "ssh2")
+		if err := k.prepFilesystem(); err != nil {
+			t.Errorf("filesystem error: %v\n", err)
+		}
+
+		// Private key
+		filePath := filepath.Join(k.KeyDir, k.Filename)
+		if !createEmptyFile(t, filePath) {
+			return
+		}
+		if err := k.WriteKeys(); err == nil {
+			t.Errorf("we wrote the private key over an existing file, but we were not supposed to")
+		}
+		if err := os.Remove(filePath); err != nil {
+			t.Errorf("could not remove file %s", filePath)
+		}
+
+		// Public key
+		if !createEmptyFile(t, filePath+".pub") {
+			return
+		}
+		if err := k.WriteKeys(); err == nil {
+			t.Errorf("we wrote the public key over an existing file, but we were not supposed to")
+		}
+	})
+}
+
 // touchTestFile is a utility function we're using in testing.
 func createEmptyFile(t *testing.T, path string) (ok bool) {
 	dir := filepath.Dir(path)
